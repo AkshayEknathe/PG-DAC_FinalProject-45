49c5a9dfcd575e4e1b72182ce30b21e5355e5443
AkshayEknathe <akshayeknathe8788@gmail.com>
2025-08-07 20:12:13 +0530

    import added

--- hospital-management-system/src/main/java/com/app/Service/AppointmentServiceImpl.java
+++ hospital-management-system/src/main/java/com/app/Service/AppointmentServiceImpl.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
